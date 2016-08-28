caba1cc3cf9411e57ab09408f5d211c5d13a3856
Andrea Moruno <andrea@genso.com.bo>
2016-08-27 21:42:01 -0400

    delete comments in ScannerParser

--- src/J2s/Integration/ScannerParser.hs
+++ src/J2s/Integration/ScannerParser.hs
@@ -5,15 +5,6 @@ import J2s.Scanner.Token
 import UU.Scanner.Position
 import UU.Parsing
 
--- import Text.ParserCombinators.UU.Core
--- import Text.ParserCombinators.UU.BasicInstances
--- import Text.ParserCombinators.UU.Derived
--- import UU.Parsing.Interface
-
---  INTEGRACION DEL SCANNER CON EL PARSER
-
--- file debe llevar el path del archivo actual
--- file = ""
 
 instance Eq J2sSc => Eq Token where
   (Token j2s1 cad1 pos1) == (Token j2s2 cad2 pos2) = j2s1 == j2s2 && (  j2s1 == Identifier ||
@@ -58,34 +49,9 @@ pOctalIntegerLiteral             =  tSym OctalIntegerLiteral ""
 pDecimalFloatingPointLiteral     =  tSym DecimalFloatingPointLiteral "" 
 pHexadecimalFloatingPointLiteral =  tSym HexadecimalFloatingPointLiteral ""
 
-{-
-pDecimalIntegerLiteral           =  sem_Integer <$> tSym DecimalIntegerLiteral ""
-pHexIntegerLiteral               =  sem_Integer <$> tSym HexIntegerLiteral ""
-pOctalIntegerLiteral             =  sem_Integer <$> tSym OctalIntegerLiteral ""
-pDecimalFloatingPointLiteral     = sem_Floating <$> tSym DecimalFloatingPointLiteral "" 
-pHexadecimalFloatingPointLiteral = sem_Floating <$> tSym HexadecimalFloatingPointLiteral ""
-
--- sem_Integer x = read x :: Integer
--- sem_Floating x = read x :: Double 
-
--}
-
 pNullLiteral      "null"   = tSym NullLiteral "null"
 
--- pIntegerLiteral              = tSym (IntegerLiteral pIntLiteral) ""
--- pIntLiteral                  = sem_IntLiteral_DecimalIntegerLiteral
---                            <|> sem_IntLiteral_HexIntegerLiteral
---                            <|> sem_IntLiteral_OctalIntegerLiteral
-
--- pDecimalIntegerLiteral =  
--- pHexIntegerLiteral     =
--- pOctalIntegerLiteral   =
-
--- sem_IntLiteral_DecimalIntegerLiteral = DecimalIntegerLiteral
--- sem_IntLiteral_HexIntegerLiteral     = HexIntegerLiteral
--- sem_IntLiteral_OctalIntegerLiteral   = OctalIntegerLiteral
--- pFloatingLiteral             = tSym (FloatingLiteral) ""
-
+-- ToDo check when operators will be tested
 -- pOperator'' ">" ">" ">" = tSym Operator "HS@J2SShift>>>ShiftJ2S@"
 -- pOperator'  ">" ">"     = tSym Operator "HS@J2SShift>>ShiftJ2s@"
 
