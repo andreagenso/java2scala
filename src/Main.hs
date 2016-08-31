10ed782185b7a662707a65dcae7e130213edb577
Andrea Moruno <andrea@genso.com.bo>
2016-08-30 22:58:54 -0400

    begin read and write file

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,14 @@ module Main where
 
 import J2s.Scanner
 import qualified AG.Sintax as AGS
+import System.Environment
+
+--import Data.Set
+import Data.Maybe
+import Text.Printf
+import System.IO
+import System.Environment
+import System.Posix.Temp
 
 testTree :: AGS.Tree
 testTree = AGS.Node (AGS.Tip 1) (AGS.Node (AGS.Tip 2)(AGS.Tip 3))
@@ -21,7 +29,22 @@ testJ2s :: String
 testJ2s = AGS.sem_J2s testJ2sImport
 
 main  :: IO()
-main =
-     do
-     putStrLn " Import result AG is .....  "
-     print (show testJ2s)
+-- main =
+--     do
+--     putStrLn " Import result AG is .....  "
+--     print (show testJ2s)
+main = do
+       g <- readJavaFile
+       let conv = testJ2s
+       writeScalaFile [conv]
+
+readJavaFile = do
+        [s] <- getArgs
+        g   <- readFile s
+        return g
+
+writeScalaFile content = do
+        (t,h) <- mkstemp "MyFile.scala"
+        mapM_ (hPutStrLn h) content
+        hClose h
+        printf "%d writing scala file  '%s'\n" (length content) t
