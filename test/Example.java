58b18ba393544922b87b5e446927a5d55c6e59ba
Andrea Moruno <andrea@genso.com.bo>
2017-01-29 18:14:21 -0400

    type parameters basic of class

--- test/Example.java
+++ test/Example.java
@@ -1,105 +1,22 @@
 package code.java;
 
-class ClaseA {
-}
-
-final class ClaseB {
-}
-
-strictfp class ClaseC {
-
-
-    protected strictfp class CB {
-    }
-
-
-    private strictfp class CC {
-    }
-
-    public static final strictfp class CD {
-    }
-
-
-    public static final strictfp
-    class CE {
-    }
-
-    public static strictfp class CF {
-    }
-
-    public abstract strictfp
-    class CG {
+class Par1<T, S> {
+    Par1(T fst, S snd) {
     }
 }
 
-
-strictfp class ClaseD {
+class Par2<T, S extends T> {
+    Par2(T fst, S snd) {}
 }
 
-
-strictfp class ClaseE {
+class Par5<T, S extends ClaseA, U> {
+    Par5(T fst, S snd, U aux) {}
 }
 
-
-final strictfp class ClaseF {
+class Par6<T, S extends ClaseA & InterfaceA & InterfaceB, U> {
+    Par6(T fst, S snd, U aux) {}
 }
 
-
-public class Example {
+class Par7<T, S extends Integer & InterfaceA & InterfaceB, U > {
+    Par7(T fst, S snd, U aux) {}
 }
-
-final strictfp class CDA {
-}
-
-
-final strictfp class CEA {
-}
-
-strictfp class CFA {
-}
-
-strictfp class CGA {
-}
-
-
-class  VVV {
-
-}
-
-// Interfaces
-
-interface InterfaceA {}
-abstract interface InterfaceB {}
-
-strictfp interface InterfaceC {
-}
-strictfp interface InterfaceD {}
-strictfp interface InterfaceE {}
-abstract  interface InterfaceF {}
-/*public interface Example {
-}*/
-
-// Definicion de interfaces genericas
-interface Par1<T, S> {}
-interface Par2<T, S extends T> {}
-interface Par3<T, S extends String> {}
-interface Par4<T, S extends Double> {}
-interface Par5<T, S extends ClaseA, U> {}
-class Par6<T, S extends ClaseA & InterfaceA & InterfaceB, U> {}
-class Par7<T, S extends Integer & InterfaceA & InterfaceB, U > {}
-
-// Herencia
-interface InterfaceBA extends InterfaceA {}
-interface ClaseBB extends code.java.InterfaceC {}
-
-
-interface CA {}
-strictfp interface CB {}
-interface CC {}
-//public static
-strictfp interface CD {}
-//public
-abstract strictfp interface CE {}
-strictfp interface CF {}
-abstract strictfp interface CG {}
-abstract interface CH {}
--- test/Example.scala
+++ test/Example.scala
@@ -1,122 +1,16 @@
 package code.java
-class ClaseA { 
+class Par1[T, S] { 
 } 
 
-final class ClaseB { 
+class Par2[T, S <: T] { 
 } 
 
-@scala.annotation.strictfp
- class ClaseC { 
+class Par5[T, S <: ClaseA, U] { 
 } 
 
-@scala.annotation.strictfp
- class ClaseD { 
+class Par6[T, S <: ClaseA with InterfaceA with InterfaceB, U] { 
 } 
 
-@scala.annotation.strictfp
- class ClaseE { 
-} 
-
-@scala.annotation.strictfp
-final class ClaseF { 
-} 
-
-class Example { 
-} 
-
-@scala.annotation.strictfp
-final class CDA { 
-} 
-
-@scala.annotation.strictfp
-final class CEA { 
-} 
-
-@scala.annotation.strictfp
- class CFA { 
-} 
-
-@scala.annotation.strictfp
- class CGA { 
-} 
-
-class VVV { 
-} 
-
-trait InterfaceA { 
-} 
-
-abstract trait InterfaceB { 
-} 
-
-@scala.annotation.strictfp
- trait InterfaceC { 
-} 
-
-@scala.annotation.strictfp
- trait InterfaceD { 
-} 
-
-@scala.annotation.strictfp
- trait InterfaceE { 
-} 
-
-abstract trait InterfaceF { 
-} 
-
-trait Par1 { 
-} 
-
-trait Par2 { 
-} 
-
-trait Par3 { 
-} 
-
-trait Par4 { 
-} 
-
-trait Par5 { 
-} 
-
-class Par6 { 
-} 
-
-class Par7 { 
-} 
-
-trait InterfaceBA { 
-} 
-
-trait ClaseBB { 
-} 
-
-trait CA { 
-} 
-
-@scala.annotation.strictfp
- trait CB { 
-} 
-
-trait CC { 
-} 
-
-@scala.annotation.strictfp
- trait CD { 
-} 
-
-@scala.annotation.strictfp
-abstract trait CE { 
-} 
-
-@scala.annotation.strictfp
- trait CF { 
-} 
-
-@scala.annotation.strictfp
-abstract trait CG { 
-} 
-
-abstract trait CH { 
+class Par7[T, S <: Integer with InterfaceA with InterfaceB, U] { 
 } 
 
