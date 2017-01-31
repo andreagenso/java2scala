91f079bb2f4a031a5567f0e959cc3e59a4697dee
Andrea Moruno <andrea@genso.com.bo>
2017-01-31 12:13:40 -0400

    inner class and tabs

---
+++ test/J2s/priority/Classes2.java
@@ -0,0 +1,36 @@
+package code.java;
+
+class ClaseA {
+}
+
+final class ClaseB {
+}
+
+strictfp class ClaseC {
+
+
+    protected strictfp class CB {
+    }
+
+
+    private strictfp class CC {
+    }
+
+    public static final strictfp class CD {
+    }
+
+
+    public static final strictfp
+    class CE {
+    }
+
+    public static strictfp class CF {
+    }
+
+    public abstract strictfp
+    class CG {
+    }
+
+    public static final class CH {
+    }
+}
