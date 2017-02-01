ea1b2d1b5cf23e32f19f6735556870dfbb5d51ef
Andrea Moruno <andrea@genso.com.bo>
2017-01-31 22:26:27 -0400

    tab fixes and examples

---
+++ test/Example.java
@@ -0,0 +1,57 @@
+package code.java;
+
+class ClaseB {}
+
+class ClaseA {
+
+    // modificadores
+    private int var1 = 1;
+    protected String var2 = "";
+    protected String var3;
+    static final double VAR4 = 0.9;
+
+    transient float var5;
+
+    static volatile int var6 = 0;
+
+    public static final ClaseB var7 = new ClaseB();
+
+    // Type
+    byte tipo1;
+    short tipo2 = 1;
+    char tipo3 = 'a';
+    int tipo4 = 3;
+    long tipo5 = 100000000;
+    float tipo6 = .2f;
+    double tipo7 = 0.009;
+    boolean tipo8 = false;
+
+    String tipo9 = "";
+
+    code.java.ClaseB tipo10 = new code.java.ClaseB();
+
+    int tipo11, tipo12, tipo13;
+
+    // expresiones de asignacion
+    String exp1 = "";
+    String exp2 = exp1;
+
+    Integer exp3 = 2;
+    Integer exp4 = exp3 + 4;
+
+    boolean exp5 = tipo8 && false;
+
+    int exp6 = (1 +2) * 50;
+
+    ClaseB exp7 = new ClaseB();
+
+    boolean d = met1();
+
+    boolean met1(){
+        return  true;
+    }
+
+    for(int i=0; i<10; i++){
+
+    }
+}
--- test/Example.scala
+++ test/Example.scala
@@ -1,2 +1,65 @@
+package code.java
+
+class ClaseB {
+
+}
+
+class ClaseA {
+
+  private var var1: Int = 0
+
+  protected var var2: String = ""
+
+  protected var var3: String = null
+
+  val VAR4: Double = 0.0
+
+  @transient var var5: Float = 0
+
+  @volatile var var6: Int = 0
+
+  val var7: ClaseB = new ClaseB()
+
+
+  var tipo1: Byte = 0
+  var tipo2: Short = 1
+  var tipo3: Char = 'a'
+  var tipo4: Int = 3
+  var tipo5: Long = 100000000
+  var tipo6: Float = .2f
+  var tipo7: Double = 0.009
+  val tipo8: Boolean = false
+
+  var tipo9: String = ""
+
+  var tipo10: code.java.ClaseB = new code.java.ClaseB()
+
+  var tipo11: Int = 0
+  var tipo12: Int = 0
+  var tipo13: Int = 0
+
+  var exp1: String = ""
+  val exp2: String = exp1 += "exp1"
+
+  var exp3: Integer = 2
+  val exp4: Integer = exp3 *= exp3
+
+  val exp5: Boolean = tipo8 && false
+
+  val exp6: Int = (1 + 2) * 50
+
+  val exp7: ClaseB = new ClaseB
+
+  val d: Boolean = met1
+
+
+  def met1: Boolean = {
+    return true
+  }
+
+
+
+}
+
 
 
