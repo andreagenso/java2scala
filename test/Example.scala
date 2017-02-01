2e2ea5f92e1deba1abff20d2783d05b36215de36
Andrea Moruno <andrea@genso.com.bo>
2017-02-01 18:39:46 -0400

    field declarations with initialzation in progress

--- test/Example.scala
+++ test/Example.scala
@@ -6,12 +6,12 @@ class ClaseB {
 
 class ClaseA { 
 
-		private var var1: Int = 0
-		protected var var2: String = null
+		private var var1: Int = 1
+		protected var var2: String = ""
 		protected var var3: String = null
-		val VAR4: Double = 0.0
+		val VAR4: Double = 0.9
 		@transient var var5: Float = 0
 		@volatile var var6: Int = 0
-		val var7: ClaseB = null}
+		val var7: ClaseB = }
 
 
