61ebadd2d9b02e44c35e997c015fe073be22368f
Andrea Moruno <andrea@genso.com.bo>
2017-02-02 15:25:14 -0400

    field declaration in progress

--- test/Example.java
+++ test/Example.java
@@ -12,45 +12,8 @@ class ClaseA {
 
     transient float var5;
 
-    static volatile int var6 = 0;
+    static volatile int var6 = 6;
 
     public static final ClaseB var7 = new ClaseB();
 
-    // Type
-    byte tipo1;
-    short tipo2 = 1;
-    char tipo3 = 'a';
-    int tipo4 = 3;
-    long tipo5 = 100000000;
-    float tipo6 = .2f;
-    double tipo7 = 0.009;
-    boolean tipo8 = false;
-
-    String tipo9 = "";
-
-    code.java.ClaseB tipo10 = new code.java.ClaseB();
-
-    int tipo11, tipo12, tipo13;
-
-    // expresiones de asignacion
-    String exp1 = "";
-    String exp2 = exp1;
-
-    Integer exp3 = 2;
-    Integer exp4 = exp3 + 4;
-
-    boolean exp5 = tipo8 && false;
-
-    int exp6 = (1 +2) * 50;
-
-    ClaseB exp7 = new ClaseB();
-
-    boolean d = met1();
-
-    boolean met1(){
-        return  true;
-    }
-
-    /*for(int i=0; i<10; i++){
-    }*/
 }
--- test/Example.scala
+++ test/Example.scala
@@ -11,7 +11,7 @@ class ClaseA {
 		protected var var3: String = null
 		val VAR4: Double = 0.9
 		@transient var var5: Float = 0
-		@volatile var var6: Int = 0
+		@volatile var var6: Int = 6
 		val var7: ClaseB = }
 
 
