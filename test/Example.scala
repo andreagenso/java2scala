2726e7f38a2b7e7555fedf2d4149ea1494a278df
Andrea Moruno <andrea@genso.com.bo>
2017-02-02 18:28:51 -0400

    fix field declaration without modifiers

--- test/Example.scala
+++ test/Example.scala
@@ -11,7 +11,34 @@ class ClaseA {
 		protected var var3: String = null
 		val VAR4: Double = 0.9
 		@transient var var5: Float = 0
-		@volatile var var6: Int = 6
-		val var7: ClaseB = }
+		@volatile var var6: Int = 0
+		var var7: ClaseB =  new ClaseB(var1,var1)
+
+		var tipo1: Byte = 0
+		var tipo2: Short = 1
+		var tipo3: Char = 'a'
+		var tipo4: Int = 3
+		var tipo5: Long = 100000000
+		var tipo6: Float = .2f
+		var tipo7: Double = 0.009
+		var tipo8: Boolean = False
+
+		var tipo9: String = ""
+		var tipo10: code =  new codejavaClaseB(var1,var1)
+		var tipo11: Int = 0
+		var tipo12: Int = 0
+		var tipo13: Int = 0
+
+	// code.java.ClaseB tipo10 = new code.java.ClaseB(var1, var1);
+
+		var exp1: String = ""
+		var exp2: String = exp1
+		var exp3: Integer = 2
+		var exp4: Integer = exp3 4
+		var exp5: Boolean = tipo8 False
+		var exp6: Int = 1 2 50
+		var exp7: ClaseB =  new ClaseB(var1,var1)
+		var d: Boolean =
 
 
+}
