24ce3cfb0566d40ddb46462357ee5a2f49e26f6f
Andrea Moruno <andrea@genso.com.bo>
2017-02-16 23:14:49 -0400

    method declaration done

--- test/Example.scala
+++ test/Example.scala
@@ -13,33 +13,6 @@ abstract class ClaseC {
 
 class ClaseA { 
 
-		private var var1: Int = 1
-		protected var var2: String = ""
-		protected var var3: String = null
-		val VAR4: Double = 0.9
-		@transient var var5: Float = 0
-		@volatile var var6: Int = 0
-		val var7: ClaseB =  new ClaseB(var1,var1)
-		var tipo1: Byte = 0
-		var tipo2: Short = 1
-		var tipo3: Char = 'a'
-		var tipo4: Int = 3
-		var tipo5: Long = 100000000
-		var tipo6: Float = .2f
-		var tipo7: Double = 0.009
-		var tipo8: Boolean = false
-		var tipo9: String = ""
-		var tipo10: code.java.ClaseB =  new code.java.ClaseB(var1,var1)
-		var tipo11: Int = 0
-		var tipo12: Int = 0
-		var tipo13: Int = 0
-		var exp1: String = ""
-		var exp2: String = exp1
-		var exp3: Integer = 2
-		var exp4: Integer = exp3 + 4
-		var exp5: Boolean = tipo8 && false
-		var exp6: Int = (1 + 2) * 50
-		var exp7: ClaseB =  new ClaseB(var1,var1)
 		var d: Boolean = met1()
 	def met1(): Boolean = {
 		true
@@ -70,10 +43,10 @@ class ClaseA {
 	private def met10(): Boolean = {
 			var var1: Boolean = true
 			var var2: Boolean = false
-				{VVVVVV
-		
-			var var3: Int = 6
-		}
+				
+		System.out.println("metodo 10")
 		(var1 && var2)
 	}
 }
+
+
