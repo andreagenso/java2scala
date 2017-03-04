87f68dd6cad6dcf4bbeab0071509ff21bdd06975
Andrea Moruno <andrea@genso.com.bo>
2017-03-04 18:17:58 -0400

    while and do while

--- test/Example.java
+++ test/Example.java
@@ -6,68 +6,8 @@ class ClaseA {
     boolean isMoving = true;
 
     void applyBrakes() {
-        if (isMoving){
+        if (isMoving)
             currentSpeed--;
-        }
     }
 
-    void applyBrakes2() {
-        if (isMoving) {
-            currentSpeed--;
-        } else {
-            System.err.println("The bicycle has already stopped!");
-        }
-    }
-
-    public void edad() {
-        int age=20;
-        if(age>18 +2){
-            System.out.print("Age is greater than 18");
-        }
-
-        if(age%18 == 0){
-            System.out.print("Age is greater than 18");
-        }
-    }
-}
-
-class IfElseDemo {
-    public void main(String[] args) {
-
-        int testscore = 76;
-        char grade;
-
-        if (testscore >= 90) {
-            grade = 'A';
-        } else if (testscore >= 80) {
-            grade = 'B';
-        } else if (testscore >= 70) {
-            grade = 'C';
-        } else if (testscore >= 60) {
-            grade = 'D';
-        } else {
-            grade = 'F';
-        }
-        System.out.println("Grade = " + grade);
-    }
-}
-
-class WhileDemo {
-    public void main(String[] args) {
-        int count = 1;
-        while (count < 11) {
-            System.out.println("Count is: " + count);
-            count++;
-        }
-    }
-}
-
-class DoWhileDemo {
-    public static void main(String[] args){
-        int count = 1;
-        do {
-            System.out.println("Count is: " + count);
-            count++;
-        } while (count < 11);
-    }
 }
--- test/Example.scala
+++ test/Example.scala
@@ -1,76 +1,13 @@
 package code.java
 
-class laseA {
+class ClaseA { 
 
 	var currentSpeed: Int = 100
 	var isMoving: Boolean = true
 	def applyBrakes(): Unit = {
-		if (isMoving) {						
+		if (isMoving) {
 			currentSpeed -= 1
 		}
-	}
-	def applyBrakes2(): Unit = {
-		if (isMoving) {						
-			currentSpeed -= 1
-		} else {						
-			System.err.println("The bicycle has already stopped!")
-		}
-	}
-	def edad(): Unit = {
-		var age: Int = 20
-		if (age > 18 + 2) {						
-			System.out.print("Age is greater than 18")
-		}
-		if (age % 18 == 0) {						
-			System.out.print("Age is greater than 18")
-		}
-	}
-}
-
-
-class IfElseDemo { 
-
-	def main(args: String): Unit = {
-		var testscore: Int = 76
-		var grade: Char = '\u0000'
-		if (testscore >= 90) {			
-			grade='A'
-		} else if (testscore >= 80) {			
-			grade='B'
-		} else if (testscore >= 70) {			
-			grade='C'
-		} else if (testscore >= 60) {						
-			grade='D'
-		} else {						
-			grade='F'
-		}
-		System.out.println("Grade = " + grade)
-	}
-}
-
-
-class WhileDemo { 
-
-	def main(args: String): Unit = {
-		var count: Int = 1
-		 while ( count < 11 ) {
-			
-			System.out.println("Count is: " + count)
-			count += 1
-		}
-	}
-}
-
-
-class DoWhileDemo { 
-
-	def main(args: String): Unit = {
-		var count: Int = 1
-				
-		System.out.println("Count is: " + count)
-		count += 1		
-		System.out.println("Count is: " + count)
-		count += 1
 	}
 }
 
