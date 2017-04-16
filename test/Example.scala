6d9b4292f27763b68f75ebecab42c7737eba6c75
Andrea Moruno <andrea@genso.com.bo>
2017-04-16 15:03:43 -0400

    for special case range Done

--- test/Example.scala
+++ test/Example.scala
@@ -1,10 +1,14 @@
 package code.java
 
-class ClaseA {
+class ClaseA { 
+
 	def forClasico(): Unit = {
+		 ---- 2
+
 		var i: Int = 0
 		while(i < 10) {			
 			System.out.println(" i es " + i)
+			System.out.println(" xxxx es " + i)
 			i += 1
 		}
 	}
