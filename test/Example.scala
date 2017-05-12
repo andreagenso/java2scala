cb381cb3b7ea1e5ff9565ce178ac39b04f03563e
Andrea Moruno <andrea@genso.com.bo>
2017-05-12 00:21:06 -0400

    switch case

--- test/Example.scala
+++ test/Example.scala
@@ -1,38 +1,58 @@
 package code.java
 
-object Switch {
-  def main(args: Array[String]) {
-    val month: Int = 8
-    var monthString: String = null
+object Switch { 
 
-    (month) match {
-      case 1 =>
-        monthString = "January"
-      case 2 =>
-        monthString = "February"
-      case 3 =>
-        monthString = "March"
-      case 4 =>
-        monthString = "April"
-      case 5 =>
-        monthString = "May"
-      case 6 =>
-        monthString = "June"
-      case 7 =>
-        monthString = "July"
-      case 8 =>
-        monthString = "August"
-      case 9 =>
-        monthString = "September"
-      case 10 =>
-        monthString = "October"
-      case 11 =>
-        monthString = "November"
-      case 12 =>
-        monthString = "December"
-      case _ =>
-        monthString = "Invalid month"
-    }
-    System.out.println(monthString)
-  }
+	def main(args: Array[String]): Unit = {
+		var month: Int = 8
+		var monthString: String = null
+		(month) match {
+			case 1 =>
+			monthString="January"
+			//todo: break is not supported
+			case 2 =>
+			monthString="February"
+			//todo: break is not supported
+			case 3 =>
+			monthString="March"
+			//todo: break is not supported
+			case 4 =>
+			monthString="April"
+			//todo: break is not supported
+			case 5 =>
+			monthString="May"
+			//todo: break is not supported
+			case 6 =>
+			monthString="June"
+			//todo: break is not supported
+			case 7 =>
+			monthString="July"
+			//todo: break is not supported
+			case 8 =>
+			monthString="August"
+			//todo: break is not supported
+			case 9 =>
+			monthString="September"
+			//todo: break is not supported
+			case 10 =>
+			monthString="October"
+			//todo: break is not supported
+			case 11 =>
+			monthString="November"
+			//todo: break is not supported
+			case 12 =>
+			monthString="December"
+			//todo: break is not supported
+			case _ =>
+			monthString="Invalid month"
+			//todo: break is not supported
+
+		}
+		System.out.println(monthString)
+	}
 }
+
+class Switch { 
+import Switch._
+}
+
+
