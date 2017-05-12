73d82ad0b9ee36506b26dd48422be18373de6308
Andrea Moruno <andrea@genso.com.bo>
2017-05-11 20:51:52 -0400

    Switch example

--- test/Example.scala
+++ test/Example.scala
@@ -1,23 +1,38 @@
 package code.java
 
-object StaticNestedClass { 
+object Switch {
+  def main(args: Array[String]) {
+    val month: Int = 8
+    var monthString: String = null
 
-	def main(args: Array[String]): Unit = {
-	}
+    (month) match {
+      case 1 =>
+        monthString = "January"
+      case 2 =>
+        monthString = "February"
+      case 3 =>
+        monthString = "March"
+      case 4 =>
+        monthString = "April"
+      case 5 =>
+        monthString = "May"
+      case 6 =>
+        monthString = "June"
+      case 7 =>
+        monthString = "July"
+      case 8 =>
+        monthString = "August"
+      case 9 =>
+        monthString = "September"
+      case 10 =>
+        monthString = "October"
+      case 11 =>
+        monthString = "November"
+      case 12 =>
+        monthString = "December"
+      case _ =>
+        monthString = "Invalid month"
+    }
+    System.out.println(monthString)
+  }
 }
-
-class StaticNestedClass { 
-import StaticNestedClass._
-	class A {
-
-	}
-
-
-	class B {
-
-	}
-
-
-}
-
-
